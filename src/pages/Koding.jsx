27d1f70e7454e5e356e5361cb7530239492ec118
dhanyn10 <dhanyn.dhan@gmail.com>
2017-11-18 19:22:10 +0700

    cara menulis kode

--- src/pages/Koding.jsx
+++ src/pages/Koding.jsx
@@ -19,6 +19,11 @@ class Koding extends React.Component
                     <CardText>
                         Masukkan kode kedalam software editor kode. 
                         Contohnya untuk membuat dokumen html, masukkan kode di bawah ini:
+                        <pre>
+                            &lt;h1&gt;Selamat datang&lt;/h1&gt;
+                        </pre>
+                        Masukkan kode di atas ke dalam kode editor, kemudian simpan dengan nama <code>index.html</code>. 
+                        Selanjutnya, buka dokumen <code>index.html</code> ini menggunakan browser.
                         <div className="form-group">
                             <a className="btn btn-success btn-sm float-right" href="https://github.com/webtutorial/builder" target="blank">edit</a>
                         </div>
